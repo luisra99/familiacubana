033cb31a4114db3961dc32ab81819189d84af01e
luisra99 <luisraul.alfonsoc@gmail.com>
2024-12-17 08:12:50 -0500

    update

--- src/_pwa-framework/gTableVirtual/GenericTableVirtualized.tsx
+++ src/_pwa-framework/gTableVirtual/GenericTableVirtualized.tsx
@@ -1,10 +1,34 @@
+import Paper from "@mui/material/Paper";
+import { TableVirtuoso } from "react-virtuoso";
+import { HeadersGroup } from "./types";
+import { VirtuosoTableComponents } from "./components/VItuoso";
+import { useFixHeader } from "./components/useFixHeader";
+import { useRow } from "./components/useRow";
 
-export default function GenericTableVirtualized() {
-  
+export default function GenericTableVirtualized({
+  headersAgrupados,
+  data,
+}: {
+  headersAgrupados: HeadersGroup;
+  data: any[];
+}) {
+  const fixedHeaderContent = useFixHeader(headersAgrupados);
+  const rowContent = useRow(headersAgrupados);
   return (
-    <
+    <Paper
+      style={{
+        height: "80vh",
+        width: "100%",
+        marginTop: "50px",
+        boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
+      }}
     >
-      
-    </>
+      <TableVirtuoso
+        data={data}
+        components={VirtuosoTableComponents}
+        fixedHeaderContent={fixedHeaderContent}
+        itemContent={rowContent}
+      />
+    </Paper>
   );
 }
