9bc4083b790d2ca1f4f58f97d9cdc8f4b2a7fb18
luisra99 <luisraul.alfonsoc@gmail.com>
2024-12-21 22:52:44 -0500

    error page

--- public/sw.js
+++ public/sw.js
@@ -1,54 +1,39 @@
-import { clientsClaim } from "workbox-core";
-import { precacheAndRoute } from "workbox-precaching";
-import { registerRoute, setCatchHandler } from "workbox-routing";
-import {
-  StaleWhileRevalidate,
-  CacheFirst,
-  NetworkFirst,
-} from "workbox-strategies";
+self.addEventListener('activate', function (event) {
+  event.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.filter(function (key) {
+      return !cacheIDs.includes(key);
+		}).map(function (key) {
+      if(!/\b(workbox)\b/.test(key))
+      return caches.delete(key);
+		}));
+	}));
+});
+
+import { clientsClaim } from 'workbox-core';
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate, CacheFirst, NetworkFirst } from 'workbox-strategies';
 
 self.__WB_MANIFEST;
 
 const version = `v4.12.2023.2${import.meta.env.ENV_SERVER_URL}`;
-const coreID = version + "_core";
-const pageID = version + "_pages";
-const imgID = version + "_assets";
-const apiID = version + "_api";
-const cacheIDs = [coreID, pageID, imgID, apiID];
+const coreID = version + '_core';
+const pageID = version + '_pages';
+const imgID = version + '_assets';
+const apiID = version + '_api';
+const cacheIDs = [coreID, pageID, imgID,apiID];
 
-// Precache the offline page
-precacheAndRoute(
-  self.__WB_MANIFEST.concat([{ url: "/offline.html", revision: null }])
-);
+precacheAndRoute(self.__WB_MANIFEST);
 self.skipWaiting();
 clientsClaim();
 
-// Clear outdated caches
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches
-      .keys()
-      .then((keys) =>
-        Promise.all(
-          keys
-            .filter(
-              (key) => !cacheIDs.includes(key) && !/\b(workbox)\b/.test(key)
-            )
-            .map((key) => caches.delete(key))
-        )
-      )
-  );
-});
-
-// Image and assets cache
 registerRoute(
   /.*\.(png|jpg|jpeg|svg|gif|webp|css)/,
   new CacheFirst({
     cacheName: imgID,
-  })
+  }),
 );
 
-// Profile route cache with data validation
 registerRoute(
   /\/profile/,
   new CacheFirst({
@@ -62,21 +47,17 @@ registerRoute(
             if (data.date !== currentDate) {
               const updatedResponse = await fetch(request);
               const updatedData = await updatedResponse.json();
-              const cache = await caches.open("dataa-cache");
-              await cache.put(
-                request,
-                new Response(JSON.stringify(updatedData))
-              );
+              const cache = await caches.open('dataa-cache');
+              await cache.put(request, new Response(JSON.stringify(updatedData)));
             }
           }
           return response;
         },
       },
     ],
-  })
+  }),
 );
 
-// API caching
 registerRoute(
   /.*\/apim\/.*/,
   new CacheFirst({
@@ -84,46 +65,19 @@ registerRoute(
     expiration: {
       maxAgeSeconds: 3000,
     },
-  })
+  }),
 );
 
 registerRoute(
   /.*\/gw\/.*/,
   new NetworkFirst({
     cacheName: apiID,
-  })
+  }),
 );
 
-// Core resources cache
 registerRoute(
   /(?!.*index.*.js$).*\/.*/,
   new StaleWhileRevalidate({
     cacheName: coreID,
-  })
+  }),
 );
-
-// Catch handler for errors
-setCatchHandler(async ({ event }) => {
-  if (event.request.destination === "document") {
-    // Return the offline page for navigation requests
-    return caches.match("/offline.html");
-  } else if (
-    event.request.destination === "script" ||
-    event.request.destination === "style"
-  ) {
-    // Optionally return a placeholder response for scripts or styles
-    return new Response(null, {
-      status: 503,
-      statusText: "Service Unavailable",
-    });
-  } else {
-    // For other types (like APIs), return a generic error response
-    return new Response(
-      JSON.stringify({ message: "Fallo conectándose con el servidor." }),
-      {
-        headers: { "Content-Type": "application/json" },
-        status: 503,
-      }
-    );
-  }
-});
